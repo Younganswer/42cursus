94118f77a55a7b219a4cfdcae45fa8af29c2d6c7
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-07-06 10:41:12 +0900

    .

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 18:35:31 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/06 00:18:42 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/07/06 10:41:02 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t sz_);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 
 // Part 2 - Additional functions
 char	*ft_substr(char const *s, unsigned int start, size_t len);
