90c8bc626ddee30501ac5f7989bac295294f3c2e
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-07-08 18:06:10 +0900

    .

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:43:11 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/06 17:16:49 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/07/08 17:36:28 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	tmp = (unsigned char *) s;
 	while (n--)
 	{
-		if (*tmp == c)
+		if (*tmp == (unsigned char ) c)
 			return ((void *) tmp);
 		tmp++;
 	}
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:27:05 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/06 19:28:23 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/07/08 18:05:12 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strnstr(const char *s, const char *target, size_t n)
 {
 	size_t	s_len;
 	size_t	target_len;
-	size_t	i;
 
 	if (!(*target))
 		return ((char *) s);
@@ -24,8 +23,7 @@ char	*ft_strnstr(const char *s, const char *target, size_t n)
 	target_len = ft_strlen(target);
 	if (s_len < target_len || n < target_len)
 		return (0);
-	i = 0;
-	while (i + n <= s_len)
+	while (target_len <= n--)
 	{
 		if (!ft_memcmp(s, target, target_len))
 			return ((char *) s);
