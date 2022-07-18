d8bd38b63eada2b27c4f5f43a45d55e66f7d16d5
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-07-18 23:02:43 +0900

    ft_printf

--- ft_printf/mandatory_srcs/ft_print_str.c
+++ ft_printf/mandatory_srcs/ft_print_str.c
@@ -6,18 +6,24 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 01:54:49 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/18 22:58:23 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/07/18 23:02:10 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+#include "../libft/includes/libft.h"
 
 int	ft_print_str(const char *str)
 {
 	int	printed;
 
 	printed = 0;
-	while (str && str[printed])
+	if (!str)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	while (str[printed])
 		printed += ft_print_char(str[printed]);
 	return (printed);
 }
