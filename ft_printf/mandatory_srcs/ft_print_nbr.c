4cadcbd7f27ecb9c05588a87d6a915414b34b421
Younganswer <younhwan@c1r6s4.42seoul.kr>
2022-07-22 14:39:25 +0900

    modify ft_print_nbr.c

--- ft_printf/mandatory_srcs/ft_print_nbr.c
+++ ft_printf/mandatory_srcs/ft_print_nbr.c
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 02:17:28 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/15 02:20:17 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/07/22 14:38:50 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_print_nbr(const int nbr)
 	int		printed;
 
 	num = ft_itoa(nbr);
+	if (!num)
+		return (0);
 	printed = ft_print_str(num);
 	free(num);
 	return (printed);
