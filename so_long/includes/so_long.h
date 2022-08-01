4e79a336d051e9bdab0f3583e6abc4cec3fa4e9f
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-08-01 14:19:34 +0900

    so_long

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 23:52:16 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/31 23:55:44 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/08/01 14:07:32 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 # include <stdlib.h>
 # include <string.h>
 
+# include "../libs/ft_printf/includes/ft_printf.h"
+# include "../libs/ft_printf/libft/includes/libft.h"
+# include "../libs/get_next_line/includes/get_next_line.h"
+
 # include "../mlx/mlx.h"
-# include "../lib/libft/includes/libft.h"
-# include "../lib/get_next_line/includes/get_next_line.h"
 
 #endif
