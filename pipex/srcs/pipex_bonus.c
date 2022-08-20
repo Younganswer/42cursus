4acecc2494e11dada4b96b3351ced168acf2c73d
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-08-20 16:11:36 +0900

    pipex

--- pipex/srcs/pipex_bonus.c
+++ pipex/srcs/pipex_bonus.c
@@ -1,105 +1,101 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipex_bonus.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: younhwan <younhwan@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 14:57:34 by younhwan          #+#    #+#             */
-/*   Updated: 2022/08/19 19:25:43 by younhwan         ###   ########.fr       */
-/*                                                                            */
+/**/
+/*:::  ::::::::   */
+/*   pipex_bonus.c  :+:  :+::+:   */
+/*+:+ +:+ +:+ */
+/*   By: younhwan <younhwan@student.42seoul.kr> +#+  +:+   +#+*/
+/*+#+#+#+#+#+   +#+   */
+/*   Created: 2022/08/19 14:57:34 by younhwan  #+##+# */
+/*   Updated: 2022/08/20 10:14:20 by younhwan ###   ########.fr   */
+/**/
 /* ************************************************************************** */
 
 #include "../includes/pipex_bonus.h"
 
-static void here_doc(int argc, const char *limiter);
-static void child_process(char **argv, int cmd_idx, char **envp);
+static void	here_doc(int argc, const char *end_flag);
+static void	get_stdin(int *fd, const char *end_flag);
+static void	child_process(int argc, char **argv, char **envp, int cmd_idx);
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    int cmd_idx;
-    int file_in;
-    int file_out;
+	int	cmd_idx;
+	int	file_in;
 
-    if (argc < 5)
+	if (argc < 5)
 		usage();
-    if (!ft_strncmp(argv[1], "here_doc", 8))
-    {
-        cmd_idx = 3;
-        file_out = open_file(argv[argc - 1], HERE_DOC);
-        here_doc(argc, argv[2]);
-    }
-    else
-    {
-        cmd_idx = 2;
-        file_out = open_file(argv[argc - 1], FILE_OUT);
-        file_in = open_file(argv[1], FILE_IN);
+	if (!ft_strncmp(argv[1], "here_doc", 8))
+	{
+		cmd_idx = 3;
+		here_doc(argc, argv[2]);
+	}
+	else
+	{
+		cmd_idx = 2;
+		file_in = open_file(argv[1], FILE_IN);
 		if (file_in != -1)
-        	dup2(file_in, STDIN_FILENO);
-    }
-    while (cmd_idx < argc - 2)
-        child_process(argv, cmd_idx++, envp);
-	if (file_out != -1)
-    	dup2(file_out, STDOUT_FILENO);
-    execute(argv, cmd_idx, envp);
-    return (0);
+			dup2(file_in, STDIN_FILENO);
+	}
+	child_process(argc, argv, envp, cmd_idx);
+	return (0);
 }
 
-static void here_doc(int argc, const char *end_flag)
+static void	here_doc(int argc, const char *end_flag)
 {
-    pid_t   reader;
-    int     fd[2];
-	char	*line;
+	pid_t	reader;
+	int		fd[2];
 
 	if (argc < 6)
 		usage();
 	if (pipe(fd) == -1)
 		exit_with_error();
 	reader = fork();
+	if (reader == -1)
+		exit_with_error();
 	if (!reader)
-	{
-		close(fd[READ]);
-		while (TRUE)
-		{
-			line = get_next_line(0);
-			if (!line)
-				break ;
-			if (!ft_strncmp(line, end_flag, ft_strlen(end_flag)))
-				exit(EXIT_SUCCESS);
-			write(fd[WRITE], line, ft_strlen(line));
-		}
-	}
+		get_stdin(fd, end_flag);
 	else
 	{
 		close(fd[WRITE]);
 		dup2(fd[READ], STDIN_FILENO);
 		wait(NULL);
 	}
-    return ;
 }
 
-static void child_process(char **argv, int cmd_idx, char **envp)
+static void	get_stdin(int *fd, const char *end_flag)
 {
-    pid_t   pid;
-    int     fd[2];
+	char	*line;
+
+	close(fd[READ]);
+	while (ft_strncmp(line, end_flag, ft_strlen(end_flag)))
+	{
+		line = get_next_line(0);
+		write(fd[WRITE], line, ft_strlen(line));
+	}
+	exit(EXIT_SUCCESS);
+}
+
+static void	child_process(int argc, char **argv, char **envp, int cmd_idx)
+{
+	pid_t	pid;
+	int		fd[2];
 
 	if (pipe(fd) == -1)
 		exit_with_error();
-    pid = fork();
+	pid = fork();
 	if (pid == -1)
 		exit_with_error();
-    if (!pid)
-    {
-        close(fd[READ]);
-        dup2(fd[WRITE], STDOUT_FILENO);
-        execute(argv, cmd_idx, envp);
-        exit(EXIT_SUCCESS);
-    }
-    else
-    {
-        close(fd[WRITE]);
-        dup2(fd[READ], STDIN_FILENO);
-        waitpid(pid, NULL, 0);
-    }
-    return ;
+	if (!pid)
+	{
+		dup2(fd[WRITE], STDOUT_FILENO);
+		if (cmd_idx == argc - 2)
+			dup2(open_file(argv[argc - 1], FILE_OUT), STDOUT_FILENO);
+		execute(argv, envp, cmd_idx);
+	}
+	else
+	{
+		dup2(fd[READ], STDIN_FILENO);
+		if (cmd_idx < argc - 2)
+			child_process(argc, argv, envp, cmd_idx + 1);
+		wait(NULL);
+	}
 }
--- pipex/srcs/pipex_utils.c
+++ pipex/srcs/pipex_utils.c
@@ -3,62 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: younhwan <younhwan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 15:14:25 by younhwan          #+#    #+#             */
-/*   Updated: 2022/08/19 19:25:36 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/08/20 16:07:17 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex_bonus.h"
 
-void		execute(char **argv, int cmd_idx, char **envp);
+void		execute(char **argv, char **envp, int cmd_idx);
 int			open_file(const char *file, t_open_flag flag);
 void		exit_with_error(void);
 void		usage(void);
 static char	*find_path(char *cmd, char **envp);
 
-void	exit_with_error(void)
-{
-	perror("\033[31mError");
-	exit(EXIT_FAILURE);
-}
-
-void	execute(char **argv, int cmd_idx, char **envp)
+void	execute(char **argv, char **envp, int cmd_idx)
 {
 	char	**cmd;
 	char	*cmd_path;
-	int		i;
+	int		fd;
 
 	cmd = ft_split(argv[cmd_idx], ' ');
 	cmd_path = find_path(cmd[0], envp);
+	fd = -1;
+	if (ft_strncmp(argv[1], "here_doc", 8))
+		fd = open(argv[1], O_RDONLY);
 	if (!cmd_path)
 	{
-		if (STDIN_FILENO != 0 || (STDIN_FILENO == 0 && 2 < cmd_idx))
+		if (fd != -1 || (fd == -1 && 2 < cmd_idx))
 		{
-			ft_putstr_fd("zsh: command not found: ", 2);
+			ft_putstr_fd("\033[31mError: command not found: ", 2);
 			ft_putstr_fd(cmd[0], 2);
-			ft_putstr_fd("\n", 2);
+			ft_putstr_fd("\033[m\n", 2);
 		}
 		exit(EXIT_FAILURE);
 	}
-	if (STDIN_FILENO != 0 || (STDIN_FILENO == 0 && 2 < cmd_idx))
+	if (fd != -1 || (fd == -1 && 2 < cmd_idx))
 		if (execve(cmd_path, cmd, envp) == -1)
 			exit_with_error();
-	free(cmd_path);
-	i = -1;
-	while (cmd[++i])
-		free(cmd[i]);
-	free(cmd);
-}
-
-void	usage(void)
-{
-	ft_putstr_fd("\033[31mError: Invalid argument\033[0m\n", 2);
-	ft_putstr_fd("Usage: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
-	ft_putstr_fd("HERE_DOC: ./pipex \"here_doc\" \
-		<LIMITER> <cmd> <cmd1> <...> <file>\n", 1);
-	exit(EXIT_SUCCESS);
 }
 
 int	open_file(const char *file, t_open_flag flag)
@@ -73,8 +56,27 @@ int	open_file(const char *file, t_open_flag flag)
 	else if (flag == FILE_IN)
 		fd = open(file, O_RDONLY, 0777);
 	if (fd == -1)
+	{
 		perror("\033[31mError");
+		if (flag == FILE_OUT)
+			exit(EXIT_FAILURE);
+	}
 	return (fd);
+}
+
+void	exit_with_error(void)
+{
+	perror("\033[31mError");
+	exit(EXIT_FAILURE);
+}
+
+void	usage(void)
+{
+	ft_putstr_fd("\033[31mError: Invalid argument\033[0m\n", 2);
+	ft_putstr_fd("Usage: ./pipex <file1> <cmd1> <cmd2> <...> <file2>\n", 1);
+	ft_putstr_fd("HERE_DOC: ./pipex \"here_doc\" \
+		<LIMITER> <cmd> <cmd1> <...> <file>\n", 1);
+	exit(EXIT_SUCCESS);
 }
 
 static char	*find_path(char *cmd, char **envp)
