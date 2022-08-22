b1d02d3a4ce4b8526223a76e30ec9d5b6c2f8ac9
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-08-22 12:20:21 +0900

    pipex

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -98,6 +98,5 @@ static void	child_process(int argc, char **argv, char **envp, int cmd_idx)
 		dup2(fd[READ], STDIN_FILENO);
 		if (cmd_idx < argc - 2)
 			child_process(argc, argv, envp, cmd_idx + 1);
-		wait(NULL);
 	}
 }
