a3dbb4eb6812c0f4128063bab800191544c8e5c7
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-08-24 01:05:58 +0900

    ft_strncmp

--- ft_printf/libft/srcs/ft_strncmp.c
+++ ft_printf/libft/srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 00:04:44 by younhwan          #+#    #+#             */
-/*   Updated: 2022/08/24 00:55:05 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/08/24 01:04:54 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*us1;
 	unsigned char	*us2;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (0 - (int) *s2);
+	if (!s2)
+		return (*s1);
 	us1 = (unsigned char *) s1;
 	us2 = (unsigned char *) s2;
 	while (n--)
 	{
-		if (*us1 - *us2 || !(*us1) || !(*us2))
+		if (*us1 - *us2)
 			return (*us1 - *us2);
+		if (!(*us1) || !(*us2))
+			break ;
 		us1++;
 		us2++;
 	}
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,23 +6,31 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 00:04:44 by younhwan          #+#    #+#             */
-/*   Updated: 2022/07/06 17:27:14 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/08/24 01:04:44 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../includes/libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned char	*us1;
 	unsigned char	*us2;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (0 - (int) *s2);
+	if (!s2)
+		return (*s1);
 	us1 = (unsigned char *) s1;
 	us2 = (unsigned char *) s2;
 	while (n--)
 	{
-		if (*us1 - *us2 || !(*us1) || !(*us2))
+		if (*us1 - *us2)
 			return (*us1 - *us2);
+		if (!(*us1) || !(*us2))
+			break ;
 		us1++;
 		us2++;
 	}
