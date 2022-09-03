183b798bb159cb1ce02743751f47e1b39bc45a30
Younganswer <younhwan@c1r5s1.42seoul.kr>
2022-09-03 15:10:58 +0900

    push_swap

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,14 +6,13 @@
 /*   By: younhwan <younhwan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:26:12 by younhwan          #+#    #+#             */
-/*   Updated: 2022/09/03 15:06:55 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/09/03 15:10:50 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/push_swap.h"
 
 static t_var	*init_var(int argc, char **argv);
-static t_arr	*read_arguments(t_stack *a, int argc, char **argv);
 static int		*stack_to_arr(t_stack *a);
 
 int	main(int argc, char **argv)
@@ -23,7 +22,7 @@ int	main(int argc, char **argv)
 	if (argc < 3)
 		ft_exit_with_error(0, EXIT_SUCCESS);
 	var = init_var(argc, argv);
-	print_stack(var->a);
+	
 	return (0);
 }
 
@@ -34,23 +33,14 @@ static t_var	*init_var(int argc, char **argv)
 	ret = (t_var *) malloc(sizeof(t_var));
 	if (!ret)
 		ft_exit_with_error(0, EXIT_FAILURE);
-	ft_memset(ret, 0, sizeof(t_var));
 	ret->a = init_stack();
 	ret->b = init_stack();
-	ret->arr = read_arguments(ret->a, argc, argv);
-	return (ret);
-}
-
-static t_arr	*read_arguments(t_stack *a, int argc, char **argv)
-{
-	t_arr	*ret;
-
-	ret = (t_arr *) malloc(sizeof(t_arr));
-	if (!ret)
+	parse_input(ret->a, argc, argv);
+	ret->arr = (t_arr *) malloc(sizeof(t_arr));
+	if (!ret->arr)
 		ft_exit_with_error(0, EXIT_FAILURE);
-	parse_input(a, argc, argv);
-	ret->chunk = stack_to_arr(a);
-	ret->sz_ = a->sz_;
+	ret->arr->chunk = stack_to_arr(ret->a);
+	ret->arr->sz_ = ret->a->sz_;
 	return (ret);
 }
 
