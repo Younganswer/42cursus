b20b7a4b26f222fa53e62caff107b3d13c7cd6bb
Younganswer <younhwan@c1r5s1.42seoul.kr>
2022-09-03 15:39:37 +0900

    push_swap

--- push_swap/incs/parse_input.h
+++ push_swap/incs/parse_input.h
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 23:29:18 by younhwan          #+#    #+#             */
-/*   Updated: 2022/09/03 15:06:34 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/09/03 15:17:15 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../libs/ft_printf/includes/ft_printf.h"
 # include "../libs/get_next_line/includes/get_next_line.h"
+# include "tools.h"
 # include "stack.h"
 
 // parse_input.c
@@ -22,6 +23,9 @@ t_bool	parse_input(t_stack *a, int argc, char **argv);
 
 // has_duplicated.c
 t_bool	has_duplicated(int *arr, int n);
+
+// init_arr.c
+t_arr	*init_arr(t_stack *a);
 
 // is_not_integer.c
 t_bool	is_not_integer(const char *input);
--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,21 +6,20 @@
 /*   By: younhwan <younhwan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:26:12 by younhwan          #+#    #+#             */
-/*   Updated: 2022/09/03 15:10:50 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/09/03 15:26:30 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/push_swap.h"
 
 static t_var	*init_var(int argc, char **argv);
-static int		*stack_to_arr(t_stack *a);
 
 int	main(int argc, char **argv)
 {
 	t_var	*var;
 
-	if (argc < 3)
-		ft_exit_with_error(0, EXIT_SUCCESS);
+	if (argc <= 2)
+		return (0);
 	var = init_var(argc, argv);
 	
 	return (0);
@@ -36,30 +35,6 @@ static t_var	*init_var(int argc, char **argv)
 	ret->a = init_stack();
 	ret->b = init_stack();
 	parse_input(ret->a, argc, argv);
-	ret->arr = (t_arr *) malloc(sizeof(t_arr));
-	if (!ret->arr)
-		ft_exit_with_error(0, EXIT_FAILURE);
-	ret->arr->chunk = stack_to_arr(ret->a);
-	ret->arr->sz_ = ret->a->sz_;
-	return (ret);
-}
-
-static int	*stack_to_arr(t_stack *a)
-{
-	t_node	*tmp;
-	int		*ret;
-	int		i;
-
-	ret = (int *) malloc(sizeof(int) * (a->sz_));
-	if (!ret)
-		ft_exit_with_error(0, EXIT_FAILURE);
-	i = 0;
-	tmp = a->head;
-	while (tmp)
-	{
-		ret[i] = tmp->val;
-		tmp = tmp->next;
-		i++;
-	}
+	ret->arr = init_arr(ret->a);
 	return (ret);
 }
