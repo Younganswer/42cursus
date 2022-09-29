4c4d2e118a68d4f01128bc3534a0b1728b58f5f6
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-09-29 16:29:34 +0900

    philo

--- philosopher/philo/srcs/utils/diff_time.c
+++ philosopher/philo/srcs/utils/diff_time.c
@@ -6,7 +6,7 @@
 /*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:04:04 by younhwan          #+#    #+#             */
-/*   Updated: 2022/09/29 16:09:14 by younhwan         ###   ########.fr       */
+/*   Updated: 2022/09/29 16:29:07 by younhwan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ size_t	diff_time(struct timeval *tv);
 size_t	diff_time(struct timeval *tv)
 {
 	struct timeval	cur;
+	size_t			ret;
 
 	gettimeofday(&cur, NULL);
-	return (cur.tv_usec - tv->tv_usec);
+	ret = (cur.tv_sec - tv->tv_sec) * 1000;
+	ret += (cur.tv_usec - tv->tv_usec) / 1000;
+	return (ret);
 }
