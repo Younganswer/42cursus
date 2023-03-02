1509c2a8557a47c3145fb67bac75e9f7529ccd20
Younganswer <dudtj1999@gmail.com>
2023-03-02 13:58:35 +0900

    .

--- CPP_Module/cpp_module_01/ex01/srcs/Zombie.cpp
+++ CPP_Module/cpp_module_01/ex01/srcs/Zombie.cpp
@@ -1,30 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: younhwan <younhwan@student.42seoul.kr>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/07 20:52:26 by younhwan          #+#    #+#             */
-/*   Updated: 2022/11/25 20:16:24 by younhwan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../incs/Zombie.hpp"
+#include <iostream>
 
-Zombie::Zombie(void) {
-	this->_name = std::string();
-	return;
-}
-
-Zombie::Zombie(const std::string &name) {
-	this->_name = std::string(name.c_str());
-	return;
-}
-
-Zombie::Zombie(const Zombie &zombie) {
-	(*this) = zombie;
-}
+Zombie::Zombie(void) { this->_name = std::string(); }
+Zombie::Zombie(const std::string &name) { this->_name = std::string(name.c_str()); }
+Zombie::Zombie(const Zombie &zombie) { (*this) = zombie; }
+Zombie::~Zombie(void) { std::cout << _name << " is destroyed\n"; }
 
 Zombie	&Zombie::operator=(const Zombie &zombie) {
 	if (this != &zombie) {
@@ -33,19 +13,8 @@ Zombie	&Zombie::operator=(const Zombie &zombie) {
 	return (*this);
 }
 
-Zombie::~Zombie(void) {
-	std::cout << _name << " is destroyed\n";
-	return;
-}
-
 // Setter
-bool	Zombie::setName(std::string name) {
-	this->_name = std::string(name.c_str());
-	return (true);
-}
+bool	Zombie::setName(std::string name) { this->_name = std::string(name.c_str()); return (true); }
 
 // Util
-void	Zombie::announce(void) {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ...\n";
-	return;
-}
+void	Zombie::announce(void) { std::cout << _name << ": BraiiiiiiinnnzzzZ...\n"; }
