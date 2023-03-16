30544bea1e0ac34e53ebef9abc34ae837275e305
Younganswer <dudtj1999@gmail.com>
2023-03-16 14:45:30 +0900

    .

--- CPP_Module/cpp_module_08/ex00/incs/easyfind.hpp
+++ CPP_Module/cpp_module_08/ex00/incs/easyfind.hpp
@@ -3,31 +3,15 @@
 
 # include <iostream>
 
-class NotFoundException: public std::exception {
-	public:
-		NotFoundException(void);
-		NotFoundException(NotFoundException const &src);
-		~NotFoundException(void) throw();
-		NotFoundException &operator=(NotFoundException const &rhs);
-		virtual const char *what() const throw();
-};
-
-NotFoundException::NotFoundException(void) {}
-NotFoundException::NotFoundException(NotFoundException const &src) { *this = src; }
-NotFoundException::~NotFoundException(void) throw() {}
-NotFoundException	&NotFoundException::operator=(NotFoundException const &rhs) {
-	(void) rhs;
-	return (*this);
-}
-const char *NotFoundException::what() const throw() {
-	return ("Not Found");
-}
-
 template <typename T>
 typename T::iterator easyfind(T &container, int n) {
-	typename T::iterator it = std::find(container.begin(), container.end(), n);
-	if (it == container.end()) {
-		throw NotFoundException();
+	typename T::iterator it = container.begin();
+
+	while (it != container.end()) {
+		if (*it == n) {
+			break;
+		}
+		it++;
 	}
 	return (it);
 }
--- CPP_Module/cpp_module_08/ex00/srcs/main.cpp
+++ CPP_Module/cpp_module_08/ex00/srcs/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+bool	easyfind_with_print(std::vector<int> &v, int target);
+
 int	main(void) {
 	{
 		std::vector<int>	v;
@@ -10,30 +12,21 @@ int	main(void) {
 		for (size_t i=0; i<10; i++) {
 			v.push_back(i);
 		}
-
-		{
-			int	toFind = 5;
-
-			std::cout << "Searching for " << toFind << " in vector" << '\n';
-			try {
-				std::vector<int>::iterator it = easyfind(v, 5);
-				std::cout << "Found: " << *it << " at index " << it - v.begin() << '\n';
-			} catch (std::exception &e) {
-				std::cout << "\033[31m" << "Error: " << e.what() << ": " << toFind << " is not in vector" << "\033[0m" << '\n';
-			}
-		}
+		easyfind_with_print(v, 5);
 		std::cout << '\n';
-		{
-			int	toFind = 10;
-
-			std::cout << "Searching for " << toFind << " in vector" << '\n';
-			try {
-				std::vector<int>::iterator it = easyfind(v, 10);
-				std::cout << "Found: " << *it << " at index " << it - v.begin() << '\n';
-			} catch (std::exception &e) {
-				std::cout << "\033[31m" << "Error: " << e.what() << ": " << toFind << " is not in vector" << "\033[0m" << '\n';
-			}
-		}
+		easyfind_with_print(v, 10);
 	}
 	return (0);
+}
+
+bool	easyfind_with_print(std::vector<int> &v, int target) {
+	std::vector<int>::iterator it = easyfind(v, target);
+
+	std::cout << "Searching for " << target << " in vector" << '\n';
+	if (it != v.end()) {
+		std::cout << "Found: " << *it << " at index " << it - v.begin() << '\n';
+	} else {
+		std::cout << "\033[31m" << "Error: " << "Not found: " << target << " is not in vector" << "\033[0m" << '\n';
+	}
+	return (true);
 }
