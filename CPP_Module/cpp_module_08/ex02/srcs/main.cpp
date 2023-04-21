9788776c54aa2e912fc7a6e397bef17241abfced
Younganswer <younganswer@kookmin.ac.kr>
2023-04-21 16:35:07 +0900

    .

--- CPP_Module/cpp_module_08/ex02/srcs/main.cpp
+++ CPP_Module/cpp_module_08/ex02/srcs/main.cpp
@@ -1,6 +1,10 @@
 #include "../incs/MutantStack.hpp"
 #include <iostream>
 #include <string>
+#include <vector>
+
+template <typename T>
+bool	printMutantStack(const MutantStack<T> &mstack);
 
 int	main(void) {
 	{
@@ -22,17 +26,7 @@ int	main(void) {
 		mstack.push(737);
 		mstack.push(0);
 		
-		MutantStack<int>::iterator	it = mstack.begin();
-
-		++it;
-		--it;
-
-		std::cout << "mstack: ";
-		while (it != mstack.end()) {
-			std::cout << *it << ' ';
-			++it;
-		}
-		std::cout << '\n';
+		printMutantStack(mstack);
 		
 		std::stack<int>	s(mstack);
 	}
@@ -56,19 +50,50 @@ int	main(void) {
 		mstack.push("42");
 		mstack.push("!");
 		
-		MutantStack<std::string>::iterator	it = mstack.begin();
+		printMutantStack(mstack);
 
-		++it;
-		--it;
-
-		std::cout << "mstack: ";
-		while (it != mstack.end()) {
-			std::cout << *it << ' ';
-			++it;
-		}
-		std::cout << '\n';
-		
 		std::stack<std::string>	s(mstack);
 	}
+	std::cout << '\n';
+	{
+		std::vector<std::string>	vec;
+
+		vec.push_back("Hello");
+		vec.push_back("World");
+
+		std::cout << "Current size of vec: " << vec.size() << '\n';
+		std::cout << "Back of vec: " << vec.back() << '\n';
+
+		vec.pop_back();
+
+		std::cout << "Current size of vec after popping: " << vec.size() << '\n';
+		std::cout << "Back of vec after popping: " << vec.back() << '\n';
+
+		vec.push_back("World");
+		vec.push_back("Hello");
+		vec.push_back("42");
+		vec.push_back("!");
+		
+		std::cout << "vec:";
+		for (std::vector<std::string>::const_iterator it=vec.begin(); it!=vec.end(); it++) {
+			std::cout << ' ' << *it;
+		}
+		std::cout << '\n';
+	}
 	return (0);
+}
+
+template <typename T>
+bool	printMutantStack(const MutantStack<T> &mstack) {
+	typename MutantStack<T>::const_iterator	it = mstack.begin();
+
+	++it;
+	--it;
+	std::cout << "mstack: ";
+	while (it != mstack.end()) {
+		std::cout << *it << ' ';
+		++it;
+	}
+	std::cout << '\n';
+	return (true);
 }
